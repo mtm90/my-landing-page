25105f26d17ac40d173a014c8272eaf6e52f9bef
mtm90 <mattia22nchi@gmail.com>
2024-05-31 11:16:10 +0200

    more changes about

--- src/components/MainAbout.jsx
+++ src/components/MainAbout.jsx
@@ -13,16 +13,16 @@ const MainAbout = () => {
         passion for new technologies like <span>blockchain</span> and <span>AI</span> led me to explore a
         career in tech. <br /><br />Due to the challenging job market in my field of study,
         I decided to pivot and acquire the skills necessary to become a
-        front-end developer. <br />I am currently honing my expertise in <span>HTML</span>, <span>CSS</span>,
+        front-end developer. <br />I am currently honing my expertise in <span>HTML</span>, <span>CSS</span>
         and <span>JavaScript</span>, with a focus on <span>React</span>. <br /><br />While I do not yet have
         professional experience in this field, I am highly motivated to start my
         career and continuously improve my skills. <br />Recently, I completed an
         intensive online course where I gained practical knowledge and hands-on
         experience.  <br /><br />I am particularly excited about this new career path because
         it allows me to leverage my creativity, adaptability, and
-        problem-solving skills. I am known for my rapid thinking and ability to
+        problem-solving skills. <br />I am known for my rapid thinking and ability to
         manage my time effectively, ensuring I meet deadlines without
-        compromising quality. My accountability and active listening skills make
+        compromising quality. <br /> My accountability and active listening skills make
         me a reliable and communicative team member, eager to contribute and
         grow.  <br /><br />If you would like to know more about my background and
         qualifications, please check my CV. <br /><br />

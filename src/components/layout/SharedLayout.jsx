d479f474a79596dd16c00aa4232c0b84f8a5cd3d
mtm90 <mattia22nchi@gmail.com>
2024-05-31 09:41:02 +0200

    shades

--- src/components/layout/SharedLayout.jsx
+++ src/components/layout/SharedLayout.jsx
@@ -9,7 +9,7 @@ const LayoutWithStyle = () => {
       <style>{`
         body { background-color: ${colors.body}; }
         header, footer, .sidebar, .main, .colors {
-          box-shadow: 1px 1px 4px 1px, -1px -1px 4px 1px ${colors.header};
+          box-shadow: 1px 1px 2px 0.5px, -1px -1px 2px 0.5px ${colors.header};
         }
       `}</style>
       <Outlet />
